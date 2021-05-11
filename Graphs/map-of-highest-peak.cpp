aac6e90f796621a027303b337839bb01202003b7
afghani-iitkgp <afghaniiit@gmail.com>
2021-05-11 14:06:57 +0530

    lx hp

--- Graphs/map-of-highest-peak.cpp
+++ Graphs/map-of-highest-peak.cpp
@@ -63,127 +63,38 @@ struct node
 };
 
 
-<<<<<<< HEAD
-vector<vector<int>> highest_peak2(vector<vector<int>> *is_water)
-{   
-    int nRow = (*is_water).size();
-    int nCol = (*is_water)[0].size();
-    node* auxArr[nRow][nCol];
-=======
 vector<vector<int>> highest_peak2(vector<vector<int>> &is_water)
-{   
-    int nRow = is_water.size();
-    int nCol = is_water[0].size();
-    node* aux[nRow][nCol];
->>>>>>> 3fe4a05ec90fd3274e3578c5bad4ba3230149c8c
-
-    queue<node*> q;
-
-    
-
-    for (int i=0; i < nRow; i++) {
-        for (int j=0; j < nCol; j++) {
-<<<<<<< HEAD
-            node* u = new node;
-            u->i = i;
-            u->j = j;
-            u->val = 0;
-            u->visited = false;
-            auxArr[i][j] = u;
-      
-            if ((*is_water)[i][j] == 1) {
-=======
-                node* u = new node;
-                u->i = i;
-                u->j = j;
-                u->val = 0;
-                u->visited = false;
-                aux[i][j] = u;
-            if (is_water[i][j] == 0) {
-                continue;
-            }        
-            else if (is_water[i][j] == 1) {
->>>>>>> 3fe4a05ec90fd3274e3578c5bad4ba3230149c8c
-                u->visited = true;
-                q.push(u);
-            }        
-        }
-    }
-
-    while (!q.empty()) {
-        node* s = q.front();
-        q.pop();
-
-<<<<<<< HEAD
-        if (s->i > 0) {
-            node* v = new node;
-
-            v->i = s->i - 1;
-            v->j = s->j;
-            if (auxArr[v->i][v->j]->visited==false) {
-                v->val = s->val + 1;
-                v->visited = true;
-                q.push(v);
-                auxArr[v->i][v->j] = v;
-                // auxArr[v->i][v->j]->visited = true;
-            }
-
-            
-        }
-
-        if (s->i < nRow-1) {                 
-            node* v = new node;
-
-            v->i = s->i + 1;
-            v->j = s->j;
-            if (auxArr[v->i][v->j]->visited==false) {
-                v->val = s->val + 1;
-                v->visited = true;
-
-                q.push(v);
-                auxArr[v->i][v->j] = v;
-                // auxArr[v->i][v->j]->visited = true;
-
-            }
-
-
-        }   
-
-        if (s->j > 0) {
-            node* v = new node;
-
-            v->i = s->i;
-            v->j = s->j - 1;
-            if (auxArr[v->i][v->j]->visited==false) {
-                v->val = s->val + 1;
-                v->visited = true;
-                q.push(v);
-                auxArr[v->i][v->j] = v;
-                // auxArr[v->i][v->j]->visited = true;
-            }
-            
-
-        } 
-
-        if (s->j < nCol-1) {
-            node* v = new node;
-
-            v->i = s->i;
-            v->j = s->j + 1;
-            if (auxArr[v->i][v->j]->visited==false) {
-                v->val = s->val + 1;
-                v->visited = true;
-                q.push(v);
-                auxArr[v->i][v->j] = v;
-                // auxArr[v->i][v->j]->visited = true;
-            }
-
-        } 
-    
         
-=======
-        if (s->visited == false or s->visited == true) {
-            // s->visited = true;
+    {   
+        int nRow = is_water.size();
+        int nCol = is_water[0].size();
+        node* aux[nRow][nCol];
+
+        queue<node*> q;
+
+
+
+        for (int i=0; i < nRow; i++) {
+            for (int j=0; j < nCol; j++) {
+                    node* u = new node;
+                    u->i = i;
+                    u->j = j;
+                    u->val = 0;
+                    u->visited = false;
+                    aux[i][j] = u;
+                if (is_water[i][j] == 0) {
+                    continue;
+                }        
+                else if (is_water[i][j] == 1) {
+                    u->visited = true;
+                    q.push(u);
+                }        
+            }
+        }
+
+        while (!q.empty()) {
+            node* s = q.front();
+            q.pop();
 
             if (s->i > 0) {
                 node* v = new node;
@@ -192,12 +103,13 @@ vector<vector<int>> highest_peak2(vector<vector<int>> &is_water)
                 v->j = s->j;
                 if (aux[v->i][v->j]->visited==false) {
                     v->val = s->val + 1;
+                    v->visited = true;
                     q.push(v);
                     aux[v->i][v->j] = v;
-                    aux[v->i][v->j]->visited = true;
+                    // aux[v->i][v->j]->visited = true;
                 }
 
-                
+
             }
 
             if (s->i < nRow-1) {                 
@@ -207,9 +119,11 @@ vector<vector<int>> highest_peak2(vector<vector<int>> &is_water)
                 v->j = s->j;
                 if (aux[v->i][v->j]->visited==false) {
                     v->val = s->val + 1;
+                    v->visited = true;
                     q.push(v);
                     aux[v->i][v->j] = v;
-                    aux[v->i][v->j]->visited = true;                    
+                    // aux[v->i][v->j]->visited = true;
+
                 }
 
 
@@ -222,11 +136,12 @@ vector<vector<int>> highest_peak2(vector<vector<int>> &is_water)
                 v->j = s->j - 1;
                 if (aux[v->i][v->j]->visited==false) {
                     v->val = s->val + 1;
+                    v->visited = true;
                     q.push(v);
                     aux[v->i][v->j] = v;
-                    aux[v->i][v->j]->visited = true;
+                    // aux[v->i][v->j]->visited = true;
                 }
-                
+
 
             } 
 
@@ -237,47 +152,37 @@ vector<vector<int>> highest_peak2(vector<vector<int>> &is_water)
                 v->j = s->j + 1;
                 if (aux[v->i][v->j]->visited==false) {
                     v->val = s->val + 1;
+                    v->visited = true;
                     q.push(v);
                     aux[v->i][v->j] = v;
-                    aux[v->i][v->j]->visited = true;
+                    // aux[v->i][v->j]->visited = true;
                 }
 
             } 
-        
+
+
         }
->>>>>>> 3fe4a05ec90fd3274e3578c5bad4ba3230149c8c
-    }
 
 
-    vector<vector<int>> vec;
-    vec.resize(nRow);
-<<<<<<< HEAD
+        vector<vector<int>> vec;
+        vec.resize(nRow);
 
 
 
-    for (int i=0; i<nRow; i++) {
-        // vec[i].resize(nCol);
+        for (int i=0; i<nRow; i++) {
+            // vec[i].resize(nCol);
 
-        for (int j=0; j<nCol; j++) {
-            cout<< auxArr[i][j]->val <<" -- ";
-            vec[i].push_back(auxArr[i][j]->val);            
-=======
-    for (int i=0; i<nRow; i++)
-        vec[i].resize(nCol);
-
-    for (int i=0; i<nRow; i++) {
-        for (int j=0; j<nCol; j++) {
-            cout<< aux[i][j]->val <<" -- ";
-            vec[i].push_back(aux[i][j]->val);
->>>>>>> 3fe4a05ec90fd3274e3578c5bad4ba3230149c8c
+            for (int j=0; j<nCol; j++) {
+                // cout<< aux[i][j]->val <<" -- ";
+                vec[i].push_back(aux[i][j]->val);            
+                // cout<< endl;
+            }
             // cout<< endl;
         }
-        cout<< endl;
+
+        return vec;
+
     }
-
-    return vec;
-
-}
 
 int main()
 {
@@ -307,26 +212,15 @@ int main()
                     {0,0,1,0,0,0,0,0}
                 };
 
-<<<<<<< HEAD
-    vector<vector<int>> v = highest_peak2(&is_water);
-    cout<<endl;
-
-    for (auto i=v.begin(); i!=v.end(); i++) {
-        for (int j : *i)
-            cout<<j<<" -- ";
-        cout<<endl;  
-
-    }
-  
-=======
     vector<vector<int>> v = highest_peak2(is_water);
 
-    for (auto x=v.begin(); x != v.end(); x++)
+    for (auto x=v.begin(); x != v.end(); x++) {
         for (auto y : *x)
-            cout<< y << "  ";
-        cout<<endl;
+            cout<< y << " | ";
+        cout<<'\n';
+    }
+
     
->>>>>>> 3fe4a05ec90fd3274e3578c5bad4ba3230149c8c
     
     return 0;
 }
